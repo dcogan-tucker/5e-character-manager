048d38b36a1358c1cb7aae1669758201d2e562b3
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-03-16 16:06:24 +0000

    first commt

---
+++ src/main/java/com/sparta/dominic/dungeonsanddragons5echaractermanager/DungeonsAndDragons5eCharacterManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.dominic.dungeonsanddragons5echaractermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonsAndDragons5eCharacterManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DungeonsAndDragons5eCharacterManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/dominic/dungeonsanddragons5echaractermanager/DungeonsAndDragons5eCharacterManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.dominic.dungeonsanddragons5echaractermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonsAndDragons5eCharacterManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
