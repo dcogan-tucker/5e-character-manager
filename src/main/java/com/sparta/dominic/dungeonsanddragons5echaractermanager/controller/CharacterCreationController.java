8081706cca6add524670c7180143ba6aa995c8b0
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-03-19 15:18:31 +0000

    added functionality to save and view characters

--- src/main/java/com/sparta/dominic/dungeonsanddragons5echaractermanager/controller/CharacterCreationController.java
+++ src/main/java/com/sparta/dominic/dungeonsanddragons5echaractermanager/controller/CharacterCreationController.java
@@ -1,20 +1,36 @@
 package com.sparta.dominic.dungeonsanddragons5echaractermanager.controller;
 
 import com.sparta.dominic.dungeonsanddragons5echaractermanager.entity.CharacterEntity;
+import com.sparta.dominic.dungeonsanddragons5echaractermanager.entity.ProficiencyEntity;
+import com.sparta.dominic.dungeonsanddragons5echaractermanager.entity.TraitEntity;
+import com.sparta.dominic.dungeonsanddragons5echaractermanager.entity.UserEntity;
 import com.sparta.dominic.dungeonsanddragons5echaractermanager.service.APIQueryService;
+import com.sparta.dominic.dungeonsanddragons5echaractermanager.service.CharacterService;
+import com.sparta.dominic.dungeonsanddragons5echaractermanager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collection;
 
 @Controller
 public class CharacterCreationController {
 
     private APIQueryService apiQueryService;
+    private CharacterService characterService;
+    private UserService userService;
 
     @Autowired
-    public CharacterCreationController(APIQueryService apiQueryService) {
+    public CharacterCreationController(APIQueryService apiQueryService, CharacterService characterService, UserService userService) {
         this.apiQueryService = apiQueryService;
+        this.characterService = characterService;
+        this.userService = userService;
     }
 
     @GetMapping("/character-creation")
@@ -26,14 +42,25 @@ public class CharacterCreationController {
         return "character-creation";
     }
 
-//    @GetMapping("/character-creation?raceSelect={raceSelect}")
-//    public String characterCreationTraitAndProficiency(@PathVariable(value = "raceSelect") String race,
-//                                                       Model model) {
-//        CharacterEntity characterEntity = new CharacterEntity();
-//        characterEntity.setRace(race);
-//        model.addAttribute("characterForm", characterEntity);
-//        System.out.println("TEST");
-//        model.addAttribute("traitSelection", apiQueryService.getTraitChoices(race));
-//        return "character-creation";
-//    }
+    @PostMapping("/character-creation")
+    public String save(@ModelAttribute("characterForm") CharacterEntity character, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "character-creation";
+        }
+        character.setUser(getCurrentUser());
+        characterService.save(character);
+        return "redirect:/welcome";
+    }
+
+    @GetMapping("/view-character")
+    public String view(Model model) {
+        Collection<CharacterEntity> characters = characterService.findByUserID(getCurrentUser());
+        model.addAttribute("characters", characters);
+        return "view-character";
+    }
+
+    private UserEntity getCurrentUser() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userService.findByUsername(userDetails.getUsername());
+    }
 }
