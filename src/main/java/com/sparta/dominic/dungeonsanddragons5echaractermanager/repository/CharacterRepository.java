4b6448b8a3a180e4d5f850bc58ce57712897da3c
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-03-19 15:20:32 +0000

    added method to find characters by user

--- src/main/java/com/sparta/dominic/dungeonsanddragons5echaractermanager/repository/CharacterRepository.java
+++ src/main/java/com/sparta/dominic/dungeonsanddragons5echaractermanager/repository/CharacterRepository.java
@@ -1,9 +1,17 @@
 package com.sparta.dominic.dungeonsanddragons5echaractermanager.repository;
 
 import com.sparta.dominic.dungeonsanddragons5echaractermanager.entity.CharacterEntity;
+import com.sparta.dominic.dungeonsanddragons5echaractermanager.entity.UserEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface CharacterRepository extends CrudRepository<CharacterEntity, Integer> {
+
+    @Query("SELECT c FROM CharacterEntity c WHERE c.user = :user")
+    Collection<CharacterEntity> findByUserID(@Param("user")UserEntity user);
 }
